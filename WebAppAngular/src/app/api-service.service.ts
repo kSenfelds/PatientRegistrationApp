613aa9980a255805efedff05949c70585a7fd9bc
kSenfelds <kr.senfelds@gmail.com>
2023-06-19 13:22:41 +0300

    added environments

--- WebAppAngular/src/app/api-service.service.ts
+++ WebAppAngular/src/app/api-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Patient } from './patient/patient.component';
 import { Doctor } from './doctor';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,27 +12,27 @@ export class ApiServiceService {
   constructor(private http: HttpClient) { }
 
   getPatientData() {
-    return this.http.get('https://localhost:7231/api/Registration/GetAllPatients');
+    return this.http.get(`${environment.apiBaseUrl}/api/Registration/GetAllPatients`);
   }
 
   getDoctorData() {
-    return this.http.get('https://localhost:7231/api/Registration/GetAllDoctors');
+    return this.http.get(`${environment.apiBaseUrl}/api/Registration/GetAllDoctors`);
   }
 
   postPatientData(patientData: Patient) {
-    return this.http.post('https://localhost:7231/api/Registration/AddPatient', patientData);
+    return this.http.post(`${environment.apiBaseUrl}/api/Registration/AddPatient`, patientData);
   }
 
   postDoctorData(doctorData: Doctor) {
-    return this.http.post('https://localhost:7231/api/Registration/AddDoctor', doctorData);
+    return this.http.post(`${environment.apiBaseUrl}/api/Registration/AddDoctor`, doctorData);
   }
 
   addPatientToDoctor(patientId: number, doctorId: number) {
-    return this.http.post('https://localhost:7231/api/Registration/AddPatientToDoctor?patientId=' + patientId + '&doctorId=' + doctorId, null);
+    return this.http.post(`${environment.apiBaseUrl}/api/Registration/AddPatientToDoctor?patientId=${patientId}&doctorId=${doctorId}`, null);
   }
 
   getPatientsByDoctorId(doctorId: number) {
-    return this.http.get('https://localhost:7231/api/Registration/GetPatientByDoctor?doctorId=' + doctorId);
+    return this.http.get(`${environment.apiBaseUrl}/api/Registration/GetPatientByDoctor?doctorId=${doctorId}`);
   }
 
   
